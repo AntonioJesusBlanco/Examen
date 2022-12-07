40f0db5e3218866e96d0717b7021129dea020b62
usuario <usuario@DESKTOP-5FAD0MS>
2022-12-07 20:22:00 +0100

    Hola me llamo Jesus
    Se ha dejado unicamente el System.out.println con el mensaje Hola me
    llamo Jesus

--- Examen/src/Main.java
+++ Examen/src/Main.java
@@ -4,14 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
-		
-<<<<<<< HEAD
-		System.out.println("Hola me llamo");
-		System.out.println("hasta luego");
-=======
+
+
 		System.out.println("Hola me llamo Jesus");
->>>>>>> branch 'master' of https://github.com/AntonioJesusBlanco/Examen.git
+
 		
 		
 	}
